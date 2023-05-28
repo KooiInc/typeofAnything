5d85e9b35ead5e9bbcc928e6d09c6caa32a84095
Renzo Kooi <renzo.kooi@gmail.com>
2023-05-28 12:13:09 +0200

    Major refactoring

--- typeofany.browser.js
+++ typeofany.browser.js
@@ -1,49 +1,48 @@
 window.IS = IS;
 
 function IS(obj, ...shouldBe) {
-  const toStringTrial = something => {
-    try { return `${something}`; }
-    catch(e) { return /a Symbol value/.test(e.message) ? `Symbol` : something }
+  const undefs = [`NaN`, `null`, `undefined`];
+  const maybeSymbol = something => typeof something === `symbol` ? Symbol : something;
+
+  return shouldBe.length > 1 ? ISOneOf(obj, ...shouldBe) : determineType();
+
+  function determineType() {
+    const objOrSymbol = maybeSymbol(obj);
+    const shouldBeLen = shouldBe.length > 0;
+    shouldBe = shouldBeLen && shouldBe.shift();
+    const shouldBeIsNothing = shouldBeLen && isNothing(shouldBe);
+
+    return shouldBeIsNothing
+      ? `${objOrSymbol}` === `${shouldBe}` :
+        obj === false
+        ? shouldBe === Boolean ? true : `Boolean` :
+          thisIs()
+
+    function thisIs() {
+      const self = obj === 0 
+        ? Number : obj === `` 
+          ? String : !objOrSymbol 
+            ? {name: `${objOrSymbol}`} : Object.getPrototypeOf(obj)?.constructor;
+
+      return shouldBe 
+        ? shouldBe === self?.__proto__ ||
+          shouldBe === self ||
+          `${shouldBe}` === self?.name 
+        : self?.name;
+    }
   }
-  const nopes = [`NaN`, `null`, `undefined`];
-  const isNothing = nothing => {
-    for (let nada of nopes) {
-      if (nada === toStringTrial(nothing)) { return true; }
+
+  function isNothing(nothing) {
+    for (let nada of undefs) {
+      if (nada === `${nothing}`) { return true; }
     }
     return false;
-  };
-  const ISOneOf = (obj, ...params) => {
+  }
+
+  function ISOneOf(obj, ...params) {
     for (let param of params) {
       if (IS(obj, param))  { return true; }
     }
     return false;
-  };
-
-  if (shouldBe.length > 1) { return ISOneOf(obj, ...shouldBe); }
-
-  const isSymbolTrial = toStringTrial(obj);
-  const shouldBeLen = shouldBe.length > 0;
-  shouldBe = shouldBeLen && shouldBe.shift();
-  
-  const objIsNothing = isNothing(isSymbolTrial);
-  const shouldBeIsNothing = isNothing(shouldBe);
-
-  if (!objIsNothing && isSymbolTrial === `Symbol`) {
-    return !shouldBeLen ? isSymbolTrial : shouldBe === Symbol;
-  }
-
-  if (objIsNothing) {
-    return shouldBeIsNothing ?
-      `${obj}` === `${shouldBe}` : shouldBe instanceof Object ?
-        false : `${obj}`;
-  }
-
-  if (obj === false) { return shouldBe === Boolean ? true : `Boolean`; }
-
-  const self = obj === 0 ? Number : obj === `` ?
-    String : !obj ?
-      {name: obj.toString()} : Object.getPrototypeOf(obj)?.constructor;
-  return shouldBe ? !!(shouldBe === self?.__proto__ ||
-    shouldBe === self ||
-    shouldBe.toString === self?.name) : self?.name;
+  } 
 }
