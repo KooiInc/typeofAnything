7953ed1d547e524b7cc6905f6e4bc4b8770440a4
Renzo Kooi <renzo.kooi@gmail.com>
2024-04-08 16:47:05 +0200

    browserscript too

--- typeofany.browser.js
+++ typeofany.browser.js
@@ -1,54 +1,54 @@
 window.IS = IS;
 
 function IS(obj, ...shouldBe) {
-  const undefs = [`NaN`, `null`, `undefined`];
   const maybeSymbol = something => typeof something === `symbol` ? Symbol : something;
-
+  
   return shouldBe.length > 1 ? ISOneOf(obj, ...shouldBe) : determineType();
-
+  
   function determineType() {
     const objOrSymbol = maybeSymbol(obj);
     const shouldBeLen = shouldBe.length > 0;
     shouldBe = shouldBeLen && shouldBe.shift();
     const inputIsNothing = isNothing(objOrSymbol);
     const shouldBeIsNothing = shouldBeLen && isNothing(shouldBe);
-
+    
     if (inputIsNothing || shouldBeIsNothing) {
-      return shouldBeIsNothing 
-        ? `${objOrSymbol}` === `${shouldBe}` 
-        : !shouldBe 
+      return shouldBeIsNothing
+        ? `${objOrSymbol}` === `${shouldBe}`
+        : !shouldBe
           ? `${objOrSymbol}` : false;
     }
-
+    
     if (Object.getPrototypeOf(objOrSymbol)?.constructor === Boolean) {
-      return !shouldBe 
-        ? `Boolean` 
+      return !shouldBe
+        ? `Boolean`
         : Object.getPrototypeOf(objOrSymbol).constructor === shouldBe;
     }
-
+    
     return thisIs();
-
+    
     function thisIs() {
       const self = obj === 0
         ? Number : obj === ``
           ? String : !objOrSymbol
             ? {name: `${objOrSymbol}`} : Object.getPrototypeOf(obj)?.constructor;
-
+      
       return shouldBe
         ? shouldBe === self ||
-          shouldBe === Object.getPrototypeOf(self) ||
-          `${shouldBe}` === self?.name
+        shouldBe === Object.getPrototypeOf(self) ||
+        `${shouldBe}` === self?.name
         : self?.name;
     }
   }
-
+  
   function isNothing(nothing) {
-    for (let nada of undefs) {
-      if (nada === `${nothing}`) { return true; }
+    try {
+      return /^(undefined|NaN|null)$/.test(`${nothing}`);
+    } catch(e) {
+      return false;
     }
-    return false;
   }
-
+  
   function ISOneOf(obj, ...params) {
     for (let param of params) {
       if (IS(obj, param))  { return true; }
