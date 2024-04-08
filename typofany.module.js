1adefe175356e625b8e335e053114f92d97e35a6
Renzo Kooi <renzo.kooi@gmail.com>
2024-04-08 15:51:15 +0200

    Update typofany.module.js
    better handling of 'nothing'

--- typofany.module.js
+++ typofany.module.js
@@ -10,13 +10,23 @@ function IS(obj, ...shouldBe) {
     const objOrSymbol = maybeSymbol(obj);
     const shouldBeLen = shouldBe.length > 0;
     shouldBe = shouldBeLen && shouldBe.shift();
+    const inputIsNothing = isNothing(objOrSymbol);
     const shouldBeIsNothing = shouldBeLen && isNothing(shouldBe);
 
-    return shouldBeIsNothing
-      ? `${objOrSymbol}` === `${shouldBe}` :
-      obj === false
-        ? shouldBe === Boolean ? true : `Boolean` :
-        thisIs()
+    if (inputIsNothing || shouldBeIsNothing) {
+      return shouldBeIsNothing 
+        ? `${objOrSymbol}` === `${shouldBe}` 
+        : !shouldBe 
+          ? `${objOrSymbol}` : false;
+    }
+
+    if (Object.getPrototypeOf(objOrSymbol)?.constructor === Boolean) {
+      return !shouldBe 
+        ? `Boolean` 
+        : Object.getPrototypeOf(objOrSymbol).constructor === shouldBe;
+    }
+
+    return thisIs();
 
     function thisIs() {
       const self = obj === 0
