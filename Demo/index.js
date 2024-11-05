509a5e0a44846aecf8c649adfeb4146477436056
Renzo Kooi <renzo.kooi@gmail.com>
2024-11-05 12:13:51 +0100

    Add examples

--- Demo/index.js
+++ Demo/index.js
@@ -11,16 +11,6 @@ const type = Symbol.type;
 const printHTML = html => html.replace(/</g, `&lt;`);
 printExamples();
 
-function SomeCTOR(something) {
-  this.something = something;
-}
-
-function someProxy() {
-  return new Proxy(new String(`hello`), {
-    get(obj, key) { return key === 'world' ? ((obj += " world"), obj) : obj[key] }
-  });
-}
-
 document.querySelectorAll(`code.block`)
   .forEach(block => {
     block.classList.remove(`block`);
@@ -60,17 +50,7 @@ function getHeader() {
     const [tru, flse, zero, not_a_nr, nil, undef, div, proxyEx] =
       [true, false, 0, +("NaN"), null, undefined, 
        document.createElement("div"), someProxy()];
-    const SharedArrayBufferTrial = maybe({
-      trial: _ => new SharedArrayBuffer(16)?.[type],
-      whenError: _ => "Tried &lt;code>new SharedArrayBuffer(16)?.[type]&lt;/code>\\
-        &lt;br>Failed=> &lt;code>SharedArrayBuffer&lt;/code> not available,\\
-        security conditions probably not met" });
-    const IteratorTrial = maybe({
-      trial: _ => Iterator.from([1,2,3]?.Type,
-      whenError: _ => 
-          "Tried &lt;code>Iterator.from([1,2,3])&lt;/code>\\
-          &lt;br>Failed => Iterator is experimental and not available in all browsers" });
-      
+    
     // a constructor
     function SomeCTOR(something) {
       this.something = something;
@@ -87,17 +67,17 @@ function codeExamples() {
   const [tru, flse, zero, not_a_nr, nil, undef, div, proxyEx] =
     [true, false, 0, +("NaN"), null, undefined, document.createElement(`div`), someProxy()];
   div.textContent = `I am div`;
-  const SharedArrayBufferTrial = maybe({
-    trial: _ => new SharedArrayBuffer(16)?.[type],
-    whenError: _ => `Tried <code>new SharedArrayBuffer(16)?.[type]</code>
-      <div>Failed => <code>SharedArrayBuffer</code> not available, security conditions probably not met</div>
-      <div>See <a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/SharedArrayBuffer"
-      >MDN</a></div>`});
-  const IteratorTrial = maybe({
-    trial: _ => `<code>Iterator.from([1,2,3]?.[type]</code> ${Iterator.from([1,2,3])?.[type]}`,
-    whenError: _ => `Tried <code>Iterator.from([1,2,3])?.[type]</code> 
-      <br>Failed => Iterator is experimental and not available in all browsers`
-  });
+  
+  function SomeCTOR(something) {
+    this.something = something;
+  }
+  
+  function someProxy() {
+    return new Proxy(new String(`hello`), {
+      get(obj, key) { return key === 'world' ? ((obj += " world"), obj) : obj[key] }
+    });
+  }
+  
   return [
     getHeader(),
     t => `<div class="normal"><b>The IS function</b></div>`,
@@ -111,7 +91,7 @@ function codeExamples() {
     _ => IS(flse),
     _ => IS({} + [], String /* ES peculiarity */),
     _ => IS(true + false, Number /* ES peculiarity */),
-    
+
     t => `<div class="normal"><b>The Object symbolic extension</b></div>`,
     _ => [][type],
     _ => [][is](Map),
@@ -121,13 +101,13 @@ function codeExamples() {
     _ => `Hello World`[is](String),
     _ => `Hello World`[is](Object),
     _ => new String(`Hello World`)[is](Object),
-    
+
     t => `<div class="normal"><b>Note</b>: one can also use <code>Symbol.type/Symbol.is</code> directly</div>`,
     _ => [][Symbol.type],
     _ => [][Symbol.is](Map),
     _ => ({})[Symbol.is](Array),
     _ => [][Symbol.is](Object /* up the prototoype chain */),
-    
+
     t => `<div class="normal"><b>The <i>static</i> Object symbolic extension</b></div>`,
     _ => Object[type](`Hello`),
     _ => Object[is]([], Map),
@@ -136,7 +116,7 @@ function codeExamples() {
     _ => Object[Symbol.is](not_a_nr, NaN),
     _ => Object[is](not_a_nr, NaN, Number),
     _ => Object[is](`Hello world`, NaN, Number, String),
-    
+
     t => `<div class="normal"><b>The wrapper <code>$Wrap</code></b></div>`,
     _ => $Wrap([])[type],
     _ => $Wrap([]).type,
@@ -150,7 +130,7 @@ function codeExamples() {
            can <i>only</i> be checked using the wrapper</div>`,
     _ => null?.[type] ?? $Wrap(null).type,
     _ => undefined?.[is](undefined) ?? $Wrap(undefined).is(undefined),
-    
+
     t => `<div class="normal"><b>The <code>maybe</code> function</b></div>`,
     _ => maybe({trial() {return {};}})[is](Object),
     _ => maybe({trial: () => $Wrap(null)}).is(null),
@@ -162,7 +142,7 @@ function codeExamples() {
     _ => maybe({trial: () => {throw new Error(`error!`);}, whenError() {return `no!`}})[is](String),
     _ => maybe({trial: () => {throw new TypeError(`no!`);}, whenError(err) {return err.name; }}),
     _ => maybe({trial: () => {throw new TypeError(`no!`);}, whenError(err) {return err.name; }})[is](String),
-    
+
     t => `<div class="normal">
             <b>Proxy</b><br>
             A <code>Proxy</code> instance has no prototype,
@@ -183,7 +163,7 @@ function codeExamples() {
     _ => proxyEx[type],
     _ => proxyEx[is](Proxy),
     _ => proxyEx[is](String),
-    
+
     // TODO
     t => xProxy.native(),
     t => `<div class="normal">When we reset <code>Proxy</code>
@@ -193,13 +173,13 @@ function codeExamples() {
     _ => new Proxy(new String(), {})[type],
     _ => new Proxy(new Date(), {})[type],
     _ => new Proxy(new Date(), {})[is](Date),
-    
+
     t => `<div class="normal"><b>Note</b>: <code>proxyEx</code> was assigned with the modified
            <code>Proxy</code> constructor, so:</div>`,
     _ => proxyEx[type],
     _ => proxyEx[is](Proxy),
     _ => proxyEx[is](String),
-    
+
     t => `<div class="normal"><b>null, undefined, true, false</b>
           <br><b>Note</b>: <code>null</code> and <code>undefined</code>
           must always be wrapped. </div>`,
@@ -217,7 +197,7 @@ function codeExamples() {
     _ => undef?.[type] ?? $Wrap(undefined)[type],
     _ => undef?.[is](undefined) ?? $Wrap(undef)[is](undefined),
     _ => undef?.[is](null, NaN) ?? $Wrap(undef)[is](null, NaN),
-    
+
     t => `<div class="normal"><b>'Nothingness'</b> (<code>isNothing</code>)<br>
           <code>isNothing</code> is a special
           function imported from the module.
@@ -241,7 +221,7 @@ function codeExamples() {
     _ => isNothing(new Date(`error!`).getTime(), true),
     _ => isNothing(new Date()),
     _ => isNothing(new Date(), true),
-    
+
     t => `<div class="normal"><b>0 (zero)</b></div>`,
     _ => zero[type],
     _ => zero[is](Boolean /* should be false */),
@@ -249,7 +229,7 @@ function codeExamples() {
     _ => zero[is](Object /* literal not Object */),
     _ => new Number(zero)[is](Number),
     _ => new Number(zero)[is](Object /*<br>&nbsp;&nbsp;Up the prototype chain, Number instance is also Object */),
-    
+
     t => `<div class="normal"><b>NaN</b></div>`,
     _ => typeof not_a_nr,
     _ => not_a_nr[is](Number /* by design we DON'T consider NaN to be Number */),
@@ -260,7 +240,7 @@ function codeExamples() {
     _ => typeof new Number(not_a_nr),
     _ => new Number(not_a_nr)[type],
     _ => new Number(not_a_nr)[is](NaN),
-    
+
     t => `<div class="normal"><b>Special cases</b>
             (<code>IS(input, {isTypes: [...types], notTypes: [...types]|defaultValue: any})</code>)
             <div>When the second parameter (or the first, using the Object symbol extension or <code>$Wrap</code>)
@@ -283,19 +263,19 @@ function codeExamples() {
     _ => IS(div, {isTypes: HTMLDivElement, notTypes: HTMLUnknownElement}),
     _ => IS(div, {isTypes: HTMLUListElement, defaultValue: printHTML(div.outerHTML)}),
     _ => IS(div, {isTypes: [undefined, null, NaN], defaultValue: printHTML(div.outerHTML)}),
-    
+
     t => xProxy.custom(),
-    t => `<div class="normal">* Rewritten <code>Proxy</code> constructor (<code>xProxy.custom()</code>)</div>`,
+    t => `<div class="normal"><b>*</b> Rewritten <code>Proxy</code> constructor (<code>xProxy.custom()</code>)</div>`,
     _ => new Proxy(new Date(), {})[type],
     _ => new Proxy(new Date(), {})[is]({isTypes: Proxy, notTypes: Date}),
     _ => new Proxy(new Date(), {})[is]({isTypes: Date, notTypes: Proxy}),
-    
+
     t => xProxy.native(),
-    t => `<div class="normal">* Native <code>Proxy</code> constructor (<code>xProxy.native()</code>)</div>`,
+    t => `<div class="normal"><b>*</b> Native <code>Proxy</code> constructor (<code>xProxy.native()</code>)</div>`,
     _ => new Proxy(new Date(), {})[type],
     _ => new Proxy(new Date(), {})[is]({isTypes: Proxy, notTypes: Date}),
     _ => new Proxy(new Date(), {})[is]({isTypes: Date, notTypes: Proxy}),
-    
+
     t => `<div class="normal"><b>More examples</b></div>`,
     _ => /[a-z]/[type],
     _ => /[a-z]/[is](RegExp),
@@ -304,28 +284,67 @@ function codeExamples() {
     _ => new SomeCTOR("yada")[type],
     _ => new SomeCTOR("yada")[is](SomeCTOR),
     _ => new SomeCTOR("yada")[is](Object /* up the prototype chain */),
-    _ => Symbol(`anything`)[type],
-    _ => div[type],
+    _ => Symbol(`me`)[is](Symbol),
     _ => div[is](Node),
     _ => div[is](HTMLElement),
     _ => div[is](HTMLDivElement),
     _ => div[is](HTMLUListElement),
     _ => div[is](HTMLUListElement, HTMLAreaElement, Node),
+    
+    t => `<div class="normal"><b>toStringTag</b>:
+            use <code>Symbol.toStringTag</code> for reporting/checking 'types'.
+          <div>A number of ES20xx Objects contain the 'well known Symbol'
+            <code>Symbol.toStringTag</code> in their prototype. Such objects
+            use that Symbol for their string representation (<code>toString</code>).
+            When available <code>IS(input)</code> or <code>[type]</code> reports
+            that as the 'type' of the input between square brackets.
+            <br>Such 'types' are <i>not always available</i> as known constructors in
+            the (global) namespace (as <code>HTMLElement</code> or <code>RegExp</code> do),
+            so you can't always use (for example)
+            <br><code>IS(new Float32Array(1), Float32Array)</code>,
+            <br>...but you <i>can</i> use a string here:
+            <br><code>IS(new Float32Array(1), "Float32Array")</code>
+            <br>See also: <a target="_blank"
+              href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Symbol/toStringTag"
+              >MDN documentation</a></div>
+          </div>`,
+    _ => div[type],
+    _ => Symbol.for(`is`)[type],
+    _ => Symbol.is[Symbol.is](Symbol /* known global constructor */),
     _ => new Intl.Collator()[type],
+    _ => new Intl.Collator()[is](`Intl.Collator`),
+    _ => new Intl.Collator()[is](Object),
     _ => new Float32Array(1)[type],
+    _ => new Float32Array(1)[is](TypedArray /* not a known global constructor */),
+    _ => new Float32Array(1)[is](Object),
+    _ => new Float32Array(1)[is](Array, "Float32Array"),
     _ => new Error(`error!`)[type],
     _ => new DataView(new ArrayBuffer(2))[type],
-    _ => function* () {
-      yield 'a';
-      yield 'b';
-      yield 'c';
-    }[type],
+    _ => function* () { yield 'a'; yield 'b'; yield 'c'; }[type],
     _ => new FinalizationRegistry(_ => {})[type],
-    _ => IteratorTrial,
+    _ => new FinalizationRegistry(_ => {})[is]("FinalizationRegistry"),
+    _ => new FinalizationRegistry(_ => {})[is](Function, "FinalizationRegistry"),
+    _ => new FinalizationRegistry(_ => {})[is](Function),
+    _ => new FinalizationRegistry(_ => {})[is](Object),
+    _ => Iterator[type],
+    _ => Iterator.from([1,2,3])[type],
+    _ => Iterator.from([1,2,3])[is](`Array Iterator`),
+    _ => Iterator.from([1,2,3])[is](Object),
     _ => new AggregateError(`aggregateError!`)[type],
-    _ => SharedArrayBufferTrial,
+    _ => new SharedArrayBuffer(16)?.[type],
+    _ => Intl[type],
+    _ => Intl[is](Object),
+    _ => Intl[is](`Intl`),
     _ => Intl.Collator[type],
+    _ => Intl.Collator[is](Function),
+    _ => JSON[type],
+    _ => JSON[is](Object),
+    _ => Math[type],
+    _ => new Promise((a, b) => {})[type],
+    _ => new Promise((a, b) => {})[is](Promise),
     _ => function* () {}[type],
+    _ => function* () {}[is](Function),
+    _ => function* () {}[is](`GeneratorFunction`),
   ]
 }
 
@@ -344,16 +363,17 @@ function wrap2Container() {
 
 function logExampleCB(example) {
   const fn = example.toString().trim();
-  const result = example();
-  return maybe({
-    trial: _ =>
-      fn.startsWith('t')
-        ? result && log(`!!${example()}`)
-        : log(toCode(String(fn).slice(4), example())),
-    whenError: err => {
-      log(`<pre>${err}</pre>`);
-      console.error(err);
-    } });
+  const whenError = err => {
+    log(`Tried: <code>${fn.slice(fn.indexOf(`>`)+1)}</code>,
+      <br>Failed with ${err.name}: "${err.message}".`);
+    return console.error(err);
+  };
+  const result = maybe({trial: example, whenError});
+  if (!isNothing(result)) {
+    return fn.startsWith('t')
+      ? result && log(`!!${example()}`)
+      : log(toCode(String(fn).slice(4), example()))
+  }
 }
 
 function printExamples() {
